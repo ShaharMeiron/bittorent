73713aeca99eb2e90743115caef64a44bd2e79c1
ShaharMeiron <shaharmeiron@gmail.com>
2025-05-21 15:03:46 +0300

    Now I'm writing my own code.

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,4 @@
-import os
 import time
 
-
-t = time.time()
-
-with open("file.txt", 'r') as file:
-    data = file.read(1)
-print(len(data))
-
-print(time.time() - t)
+creation_date = int(time.time())
+print(creation_date)
